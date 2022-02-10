44cfc932060ddc3f65f00b42533609d4fc57dfad
Pavel Novikov <paaaaaaaasha@gmail.com>
2022-02-10 16:06:07 +0700

    commit 2

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
-
 def create_list(folder):
     file_list = os.listdir(folder)
-    tmp_list = {}
+    sorted_file_list = []
     for file in file_list:
-        with open('file', 'r') as f:
-            t = f.read().count('')
+        with open(folder + "/" + file) as _tmp_list:
+            sorted_file_list.append([file, 0, []])
+            for line in _tmp_list:
+                sorted_file_list[-1][2].append(line.strip())
+                sorted_file_list[-1][1] += 1
+    sorted_file_list=sorted(sorted_file_list, key=lambda x: x[1], reverse=False)
+    with open('by_sorted' + '.txt', 'w+') as sorted_file:
+        sorted_file.write(f'Даны файлы:\n')
+        for file in sorted_file_list:
+            sorted_file.write(f'Назввание файла: {file[0]}\n')
+            sorted_file.write(f'Количество строк: {file[1]}\n')
+            for string in file[2]:
+                sorted_file.write(string + '\n')
+            sorted_file.write('\n')
+    return print('File sorted')
 
-        print(t)
-    return print(tmp_list)
 create_list('LIST')
